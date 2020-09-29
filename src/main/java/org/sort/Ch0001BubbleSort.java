56d9929f7755e628c5bb03a777ab75ac5af327b5
wangrui <wangrui@limikeji.com>
2020-09-29 15:28:12 +0800

    类型：冒泡排序 说明：冒泡排序

--- src/main/java/org/sort/Ch0001BubbleSort.java
+++ src/main/java/org/sort/Ch0001BubbleSort.java
@@ -9,6 +9,11 @@ import java.util.Arrays;
  * 冒泡排序，从小到大排序；(大的数据先浮出来)
  * <p>
  * 掌握思想
+ * <p>
+ * 算法分析
+ * <p>
+ * 最佳情况：T(n) = O(n)   最差情况：T(n) = O(n2)   平均情况：T(n) = O(n2)
+ * <p>
  */
 public class Ch0001BubbleSort {
 
---
+++ src/main/java/org/sort/Ch0002SelectionSort.java
@@ -0,0 +1,18 @@
+package org.sort;
+
+/**
+ * @author: wangrui
+ * @date: 2020/9/29
+ * <p>
+ * 冒泡排序，从小到大排序；(大的数据先浮出来)
+ * <p>
+ * 掌握思想
+ * <p>
+ * 算法分析
+ * <p>
+ * 最佳情况：T(n) = O(n)   最差情况：T(n) = O(n2)   平均情况：T(n) = O(n2)
+ * <p>
+ */
+public class Ch0002SelectionSort {
+
+}
