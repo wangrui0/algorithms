84beaffc05ed1d6c9d217b1f0e38bee2f389976c
wangrui <wangrui@limikeji.com>
2020-11-27 17:28:57 +0800

    类型：杨辉三角中 说明：杨辉三角中

--- src/main/java/org/leetcode/Ch0118PascalsTriangle.java
+++ src/main/java/org/leetcode/Ch0118PascalsTriangle.java
@@ -1,5 +1,8 @@
 package org.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * <p>
  * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
@@ -30,4 +33,74 @@ package org.leetcode;
  */
 public class Ch0118PascalsTriangle {
 
+  public static void main(String[] args) {
+    List<List<Integer>> result = generate(5);
+    System.out.println(result);
+  }
+
+  /**
+   * 自己
+   *
+   * @param numRows
+   * @return
+   */
+  public static List<List<Integer>> generate(int numRows) {
+    List<List<Integer>> result = new ArrayList<>();
+
+    for (int i = 1; i <= numRows; i++) {
+      List<Integer> columns = new ArrayList<>();
+      for (int j = 0; j < i; j++) {
+        if (j == 0 || j == i - 1) {
+          columns.add(1);
+        } else {
+          List<Integer> beforeColumn = result.get(i - 2);
+          Integer num = beforeColumn.get(j) + beforeColumn.get(j - 1);
+          columns.add(num);
+        }
+      }
+      result.add(columns);
+    }
+    return result;
+  }
+
+  /**
+   * 官方
+   *
+   * @param numRows
+   * @return
+   */
+  public List<List<Integer>> generate2(int numRows) {
+    List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+
+    // First base case; if user requests zero rows, they get zero rows.
+    if (numRows == 0) {
+      return triangle;
+    }
+
+    // Second base case; first row is always [1].
+    triangle.add(new ArrayList<>());
+    triangle.get(0).add(1);
+
+    for (int rowNum = 1; rowNum < numRows; rowNum++) {
+      List<Integer> row = new ArrayList<>();
+      List<Integer> prevRow = triangle.get(rowNum - 1);
+
+      // The first row element is always 1.
+      row.add(1);
+
+      // Each triangle element (other than the first and last of each row)
+      // is equal to the sum of the elements above-and-to-the-left and
+      // above-and-to-the-right.
+      for (int j = 1; j < rowNum; j++) {
+        row.add(prevRow.get(j - 1) + prevRow.get(j));
+      }
+
+      // The last row element is always 1.
+      row.add(1);
+
+      triangle.add(row);
+    }
+
+    return triangle;
+  }
 }
---
+++ src/main/java/org/leetcode/Ch0119PascalsTriangleII.java
@@ -0,0 +1,66 @@
+package org.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+ * <p>        1
+ * <p>      1   1
+ * <p>     1  2  1
+ * <p>    1  3  3   1
+ * <p>  1  4  6  4   1
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 3 输出: [1,3,3,1] 进阶：
+ * <p>
+ * 你可以优化你的算法到 O(k) 空间复杂度吗？
+ * <p>
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/pascals-triangle-ii 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author: wangrui
+ * @date: 2020/11/27
+ */
+public class Ch0119PascalsTriangleII {
+
+  public static void main(String[] args) {
+    List<Integer> row = getRow2(4);
+    System.out.println(row);
+  }
+
+  public static List<Integer> getRow(int rowIndex) {
+    List<Integer> preList;
+    List<Integer> currList = new ArrayList<>();
+
+    for (int i = 0; i <= rowIndex; i++) {
+      preList = currList;
+      currList = new ArrayList<>();
+      for (int j = 0; j <= i; j++) {
+        if (j == 0 || j == i) {
+          currList.add(1);
+        } else {
+          Integer num = preList.get(j) + preList.get(j - 1);
+          currList.add(num);
+        }
+      }
+    }
+    return currList;
+  }
+
+  /**
+   * 方法二： 官方：秒啊，从后往前，太棒了。要记住这种思想，从后往前，充分利用数据的思想
+   */
+  public static List<Integer> getRow2(int rowIndex) {
+    List<Integer> cur = new ArrayList<>();
+    cur.add(1);
+    for (int i = 1; i <= rowIndex; i++) {
+      for (int j = i - 1; j > 0; j--) {
+        cur.set(j, cur.get(j - 1) + cur.get(j));
+      }
+      cur.add(1);//补上每层的最后一个 1
+    }
+    return cur;
+  }
+}
