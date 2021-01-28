aa8b72a2338004d7ac0e506d06b011f834bc7223
wangrui <wangrui@limikeji.com>
2021-01-28 19:21:27 +0800

    类型:反转一个单链表。

--- src/main/java/org/leetcode/easy/Ch0228SummaryRanges.java
+++ src/main/java/org/leetcode/easy/Ch0228SummaryRanges.java
@@ -1,5 +1,8 @@
 package org.leetcode.easy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *  <p> 给定一个无重复元素的有序整数数组 nums 。i
  *
@@ -66,5 +69,46 @@ package org.leetcode.easy;
  * @date: 2021/1/27
  */
 public class Ch0228SummaryRanges {
-
+  /**
+   * <p> 我们从数组的位置 0 出发，向右遍历。每次遇到相邻元素之间的差值大于 1 时，我们就找到了一个区间。遍历完数组之后，就能得到一系列的区间的列表。
+   *
+   * <p> 在遍历过程中，维护下标 low 和 high 分别记录区间的起点和终点，对于任何区间都有 low≤high。当得到一个区间时，根据 low 和 high 的值生成区间的字符串表示。
+   *
+   * <p>     当 low<high 时，区间的字符串表示为 ‘‘low→high"；
+   *
+   * <p>     当 low=high 时，区间的字符串表示为 ‘‘low"。
+   *
+   * <p> 作者：LeetCode-Solution
+   * <p> 链接：https://leetcode-cn.com/problems/summary-ranges/solution/hui-zong-qu-jian-by-leetcode-solution-6zrs/
+   * <p> 来源：力扣（LeetCode）
+   * <p> 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   * <p> ========================================
+   * <p> 复杂度分析
+   *
+   * <p>     时间复杂度：O(n)，其中 nnn 为数组的长度。
+   *
+   * <p>     空间复杂度：O(1)。除了用于输出的空间外，额外使用的空间为常数。
+   *
+   * @param nums
+   * @return
+   */
+  public List<String> summaryRanges2(int[] nums) {
+    List<String> result = new ArrayList<String>();
+    int i = 0;
+    int n = nums.length;
+    while (i < n) {
+      int low = i;
+      i++;
+      while (i < n && nums[i] == nums[i - 1] + 1) {
+        i++;
+      }
+      int height = i - 1;
+      StringBuilder sb = new StringBuilder(String.valueOf(nums[low]));
+      if (low < height) {
+        sb.append("->").append(String.valueOf(nums[height]));
+      }
+      result.add(sb.toString());
+    }
+    return result;
+  }
 }
