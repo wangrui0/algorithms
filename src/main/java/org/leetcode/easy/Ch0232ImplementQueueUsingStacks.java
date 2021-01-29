391326fdcb89a9ef258c7065dc4f3c20faf9d9f2
wangrui <wangrui@limikeji.com>
2021-01-29 11:47:35 +0800

    类型:反转一个单链表。

--- src/main/java/org/leetcode/easy/Ch0232ImplementQueueUsingStacks.java
+++ src/main/java/org/leetcode/easy/Ch0232ImplementQueueUsingStacks.java
@@ -1,6 +1,8 @@
 package org.leetcode.easy;
 
 
+import java.util.Stack;
+
 /**
  * <p> 232. 用栈实现队列
  *
@@ -62,6 +64,16 @@ package org.leetcode.easy;
  */
 public class Ch0232ImplementQueueUsingStacks {
 
+  private Stack<Integer> s1 = new Stack<>();
+  private Stack<Integer> s2 = new Stack<>();
+  private int front;
+
+  // Push element x to the back of queue.
+  public void push(int x) {
+    if (s1.empty())
+      front = x;
+    s1.push(x);
+  }
 
 
 }
