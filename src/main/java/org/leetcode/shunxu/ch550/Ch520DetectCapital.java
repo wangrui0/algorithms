8972fa139a02c044bc28c09aabd57784139cfc66
1176306419@qq.com <1176306419@qq.com>
2021-03-16 22:17:54 +0800

    键盘

--- src/main/java/org/leetcode/shunxu/ch550/Ch520DetectCapital.java
+++ src/main/java/org/leetcode/shunxu/ch550/Ch520DetectCapital.java
@@ -1,5 +1,7 @@
 package org.leetcode.shunxu.ch550;
 
+import com.sun.xml.internal.xsom.XSUnionSimpleType;
+
 /**
  * <p>520. 检测大写字母
  * <p>给定一个单词，你需要判断单词的大写使用是否正确。
@@ -17,6 +19,7 @@ package org.leetcode.shunxu.ch550;
  * <p>输出: True
  * <p>示例 2:
  *
+ *
  * <p>输入: "FlaG"
  * <p>输出: False
  * <p>注意: 输入是由大写和小写拉丁字母组成的非空单词。
@@ -29,7 +32,40 @@ package org.leetcode.shunxu.ch550;
  * @Date: 2021/3/15 8:23 下午
  */
 public class Ch520DetectCapital {
-    public boolean detectCapitalUse(String word) {
-        return true;
+    public static void main(String[] args) {
+        System.out.println(detectCapitalUse("FlaG"));
+    }
+
+    public static boolean detectCapitalUse(String word) {
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) >= 'A' && word.charAt(i) <= 'Z') {
+                count++;
+            }
+        }
+        if (count == 1 && word.charAt(0) >= 'A' && word.charAt(0) <= 'Z' || count == word.length()) {
+            return true;
+        } else if (count == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean detectCapitalUse2(String word) {
+        boolean res = false;
+        int count = 0;
+        char x;
+        for (int i = 0; i < word.length(); i++) {
+            x = word.charAt(i);
+            if(x >= 65 && x <= 90) {
+                count++;
+            }
+        }
+        if ((count == 1 && (word.charAt(0) >= 65 && word.charAt(0) <= 90)) || (count == word.length())) {
+            res = true;
+        } else if (count == 0) {
+            res = true;
+        }
+        return res;
     }
 }
