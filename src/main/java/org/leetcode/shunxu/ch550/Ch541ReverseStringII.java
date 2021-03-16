e212112af868a03653c10ad0d43296978333022b
1176306419@qq.com <1176306419@qq.com>
2021-03-16 22:37:22 +0800

    键盘

--- src/main/java/org/leetcode/shunxu/ch550/Ch541ReverseStringII.java
+++ src/main/java/org/leetcode/shunxu/ch550/Ch541ReverseStringII.java
@@ -26,7 +26,50 @@ package org.leetcode.shunxu.ch550;
  * @Date: 2021/3/15 8:17 下午
  */
 public class Ch541ReverseStringII {
+    public static void main(String[] args) {
+
+    }
+
     public String reverseStr(String s, int k) {
-        return null;
+        char[] chars = s.toCharArray();
+        int num = s.length();
+        for (int start = 0; start < num; start += 2 * k) {
+            int i = start;
+            int j = Math.min(i + k - 1, num - 1);
+            while (i < j) {
+                char temp = chars[i];
+                chars[i++] = chars[j];
+                chars[j--] = temp;
+            }
+        }
+        return new String(chars);
+    }
+
+    /**
+     * <p>复杂度分析
+     *
+     * <p>时间复杂度：O(N)，其中 N 是 s 的大小。我们建立一个辅助数组，用来翻转 s 的一半字符。
+     * <p>空间复杂度：O(N)，a 的大小。
+     *
+     * <p>作者：LeetCode
+     * <p>链接：https://leetcode-cn.com/problems/reverse-string-ii/solution/fan-zhuan-zi-fu-chuan-ii-by-leetcode/
+     * <p>来源：力扣（LeetCode）
+     * <p>著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param s
+     * @param k
+     * @return
+     */
+    public String reverseStr2(String s, int k) {
+        char[] a = s.toCharArray();
+        for (int start = 0; start < a.length; start += 2 * k) {
+            int i = start, j = Math.min(start + k - 1, a.length - 1);
+            while (i < j) {
+                char tmp = a[i];
+                a[i++] = a[j];
+                a[j--] = tmp;
+            }
+        }
+        return new String(a);
     }
 }
