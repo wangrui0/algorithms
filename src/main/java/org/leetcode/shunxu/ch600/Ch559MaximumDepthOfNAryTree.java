ee99b48047be943f34d39b4cc3930bcaba9aa9f5
wangrui <wangrui@limikeji.com>
2021-03-17 14:15:22 +0800

    提交

--- src/main/java/org/leetcode/shunxu/ch600/Ch559MaximumDepthOfNAryTree.java
+++ src/main/java/org/leetcode/shunxu/ch600/Ch559MaximumDepthOfNAryTree.java
@@ -1,7 +1,13 @@
 package org.leetcode.shunxu.ch600;
 
 
-import org.Node;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import javafx.util.Pair;
+import org.junit.platform.commons.util.CollectionUtils;
+import org.leetcode.Node;
 
 /**
  * <p>559. N 叉树的最大深度
@@ -39,7 +45,89 @@ import org.Node;
  * @Date: 2021/3/15 8:07 下午
  */
 public class Ch559MaximumDepthOfNAryTree {
-    public int maxDepth(Node root) {
-        return 0;
+
+  /**
+   * slef
+   *
+   * @param root
+   * @return
+   */
+  public int maxDepth(Node root) {
+    if (root == null) {
+      return 0;
     }
+    Queue<Pair<Node, Integer>> queue = new LinkedList<Pair<Node, Integer>>();
+    queue.add(new Pair(root, 1));
+    int depth = 0;
+    while (!queue.isEmpty()) {
+      Pair<Node, Integer> poll = queue.poll();
+      root = poll.getKey();
+      int val = poll.getValue();
+      if (root != null) {
+        depth = Math.max(depth, val);
+        for (Node node : root.children) {
+          queue.add(new Pair(node, val + 1));
+        }
+      }
+    }
+    return depth;
+  }
+
+  /**
+   * <p>复杂度分析
+   *
+   * <p> 时间复杂度：每个节点遍历一次，所以时间复杂度是O(N)，其中 N 为节点数。
+   *
+   * <p> 空间复杂度：最坏情况下, 树完全非平衡，
+   * <p>例如 每个节点有且仅有一个孩子节点，递归调用会发生 N 次（等于树的深度），所以存储调用栈需要 O(N)。
+   * <p>但是在最好情况下（树完全平衡），树的高度为 log(N)。
+   * <p>所以在此情况下空间复杂度为 O(log(N))。
+   *
+   * @param root
+   * @return
+   */
+  public int maxDepth2(Node root) {
+    if (root == null) {
+      return 0;
+    } else if (root.children.isEmpty()) {
+      return 1;
+    } else {
+      List<Integer> heights = new LinkedList<>();
+      for (Node item : root.children) {
+        heights.add(maxDepth(item));
+      }
+      return Collections.max(heights) + 1;
+    }
+  }
+
+  /**
+   * <p>复杂度分析
+   *
+   * <p>   时间复杂度：O(N)。
+   * <p>   空间复杂度：O(N)。
+   *
+   * @param root
+   * @return
+   */
+  public int maxDepth3(Node root) {
+    Queue<Pair<Node, Integer>> queue = new LinkedList<>();
+    if (root != null) {
+      queue.add(new Pair(root, 1));
+    }
+
+    int depth = 0;
+    while (!queue.isEmpty()) {
+      Pair<Node, Integer> current = queue.poll();
+      root = current.getKey();
+      int currentDepth = current.getValue();
+      if (root != null) {
+        depth = Math.max(depth, currentDepth);
+        for (Node c : root.children) {
+          queue.add(new Pair(c, currentDepth + 1));
+        }
+      }
+    }
+    return depth;
+  }
 }
+
