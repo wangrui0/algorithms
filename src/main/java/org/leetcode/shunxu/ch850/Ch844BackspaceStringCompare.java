add4d916e5449802b4a65d8f7e8025ee37680120
wangrui <wangrui@limikeji.com>
2021-03-30 22:37:11 +0800

    提交

--- src/main/java/org/leetcode/shunxu/ch850/Ch844BackspaceStringCompare.java
+++ src/main/java/org/leetcode/shunxu/ch850/Ch844BackspaceStringCompare.java
@@ -51,7 +51,138 @@ package org.leetcode.shunxu.ch850;
  */
 public class Ch844BackspaceStringCompare {
 
-  public boolean backspaceCompare(String S, String T) {
+  public static void main(String[] args) {
+    System.out.println(backspaceCompare("bxj##tw",
+        "bxj###tw"
+    ));
+  }
+
+  public static boolean backspaceCompare(String S, String T) {
+    int si = S.length() - 1;
+    int ti = T.length() - 1;
+    int skipS = 0;
+    int skipT = 0;
+    while (si >= 0 || ti >= 0) {
+      while (si >= 0) {
+        if (S.charAt(si) == '#') {
+          si--;
+          skipS++;
+        } else if (skipS > 0) {
+          si--;
+          skipS--;
+        } else {
+          break;
+        }
+      }
+      while (ti >= 0) {
+        if (T.charAt(ti) == '#') {
+          ti--;
+          skipT++;
+        } else if (skipT > 0) {
+          ti--;
+          skipT--;
+        } else {
+          break;
+        }
+      }
+      if (si >= 0 && ti >= 0) {
+        if (S.charAt(si) != T.charAt(ti)) {
+          return false;
+        }
+      } else if (si >= 0 || ti >= 0) {
+        return false;
+      } else {
+        return true;
+      }
+      si--;
+      ti--;
+    }
     return true;
   }
+
+  /**
+   * <p> 复杂度分析
+   *
+   * <p>    时间复杂度：O(N+M)，其中 N 和 M 分别为字符串 S 和 T 的长度。我们需要遍历两字符串各一次。
+   *
+   * <p>  空间复杂度：O(N+M)，其中 N 和 M 分别为字符串 S 和 T 的长度。主要为还原出的字符串的开销。
+   *
+   * @param S
+   * @param T
+   * @return
+   */
+  public boolean backspaceCompare_1(String S, String T) {
+    return build(S).equals(build(T));
+  }
+
+  public String build(String str) {
+    StringBuffer ret = new StringBuffer();
+    int length = str.length();
+    for (int i = 0; i < length; ++i) {
+      char ch = str.charAt(i);
+      if (ch != '#') {
+        ret.append(ch);
+      } else {
+        if (ret.length() > 0) {
+          ret.deleteCharAt(ret.length() - 1);
+        }
+      }
+    }
+    return ret.toString();
+  }
+
+
+  /**
+   * <p> 复杂度分析
+   *
+   * <p>  时间复杂度：O(N+M)，其中 N 和 M 分别为字符串 S 和 T 的长度。我们需要遍历两字符串各一次。
+   *
+   * <p>空间复杂度：O(1)。对于每个字符串，我们只需要定义一个指针和一个计数器即可
+   *
+   * @param S
+   * @param T
+   * @return
+   */
+  public boolean backspaceCompare_2(String S, String T) {
+    int i = S.length() - 1, j = T.length() - 1;
+    int skipS = 0, skipT = 0;
+
+    while (i >= 0 || j >= 0) {
+      while (i >= 0) {
+        if (S.charAt(i) == '#') {
+          skipS++;
+          i--;
+        } else if (skipS > 0) {
+          skipS--;
+          i--;
+        } else {
+          break;
+        }
+      }
+      while (j >= 0) {
+        if (T.charAt(j) == '#') {
+          skipT++;
+          j--;
+        } else if (skipT > 0) {
+          skipT--;
+          j--;
+        } else {
+          break;
+        }
+      }
+      if (i >= 0 && j >= 0) {
+        if (S.charAt(i) != T.charAt(j)) {
+          return false;
+        }
+      } else {
+        if (i >= 0 || j >= 0) {
+          return false;
+        }
+      }
+      i--;
+      j--;
+    }
+    return true;
+  }
+
 }
