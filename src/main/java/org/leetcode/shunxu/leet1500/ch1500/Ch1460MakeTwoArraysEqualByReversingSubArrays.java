40a009d793f2602b2d7c15e12fefb43d82a7f4ea
wangrui <wangrui@limikeji.com>
2021-04-22 19:00:56 +0800

    提交

--- src/main/java/org/leetcode/shunxu/leet1500/ch1500/Ch1460MakeTwoArraysEqualByReversingSubArrays.java
+++ src/main/java/org/leetcode/shunxu/leet1500/ch1500/Ch1460MakeTwoArraysEqualByReversingSubArrays.java
@@ -104,6 +104,21 @@ public class Ch1460MakeTwoArraysEqualByReversingSubArrays {
     return true;
   }
 
+  public boolean canBeEqual_4(int[] target, int[] arr) {
+    int[] targetCount = new int[1001];
+    int[] arrCount = new int[1001];
+    for (int i = 0; i < 1001; i++) {
+      targetCount[target[i]]++;
+      arrCount[arrCount[i]]++;
+    }
+    for (int i = 0; i < 1001; i++) {
+      if (targetCount[i] != arrCount[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 
 }
 
