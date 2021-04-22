05fc3d6702100a2d7c85696a369a77e543675aed
wangrui <wangrui@limikeji.com>
2021-04-22 17:14:15 +0800

    提交

--- src/main/java/org/leetcode/shunxu/leet1500/ch1450/Ch1450NumberOfStudentsDoingHomeworkAtAGivenTime.java
+++ src/main/java/org/leetcode/shunxu/leet1500/ch1450/Ch1450NumberOfStudentsDoingHomeworkAtAGivenTime.java
@@ -58,4 +58,13 @@ package org.leetcode.shunxu.leet1500.ch1450;
  */
 public class Ch1450NumberOfStudentsDoingHomeworkAtAGivenTime {
 
+  public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+    int ans = 0;
+    for (int i = 0; i < startTime.length; i++) {
+      if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+        ans++;
+      }
+    }
+    return ans;
+  }
 }
