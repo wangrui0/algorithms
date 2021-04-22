bd6a00217f2eac37f3fc93bb848587f3d794d9b4
wangrui <wangrui@limikeji.com>
2021-04-22 15:46:31 +0800

    提交

--- src/main/java/org/leetcode/shunxu/leet1500/ch1450/Ch1446ConsecutiveCharacters.java
+++ src/main/java/org/leetcode/shunxu/leet1500/ch1450/Ch1446ConsecutiveCharacters.java
@@ -51,4 +51,48 @@ package org.leetcode.shunxu.leet1500.ch1450;
  */
 public class Ch1446ConsecutiveCharacters {
 
+  public int maxPower(String s) {
+    int i = 0;
+    int j = 1;
+    int count = 1;
+    int max = Integer.MIN_VALUE;
+    if (s.length() == 1) {
+      return 1;
+    }
+    boolean flag = true;
+    while (j < s.length()) {
+      //这一步为了解决j是最后一个字符
+      if (s.charAt(i) == s.charAt(j)) {
+        count++;
+        if (j == s.length() - 1) {
+          max = Math.max(count, max);
+        }
+        j++;
+        //如果不相等,就让i指向j,j++
+      } else {
+        max = Math.max(count, max);
+        i = j;
+        j++;
+        count = 1;
+      }
+    }
+
+    return max;
+  }
+
+  public static int maxPower_self(String s) {
+    int max = 1;
+    int pre = 0;
+    for (int i = 1; i < s.length(); i++) {
+      if (s.charAt(i) != s.charAt(i - 1)) {
+        max = Math.max(max, i - pre);
+        pre = i;
+      } else if (i == s.length() - 1) {
+        max = Math.max(max, i - pre + 1);
+      }
+    }
+    return max;
+  }
+
+
 }
