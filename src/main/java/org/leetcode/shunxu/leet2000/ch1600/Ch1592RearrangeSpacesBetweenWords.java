bc9b44d2715b4e2ab2fc4147302fcd69859a5e4f
wangrui <wangrui@limikeji.com>
2021-04-24 19:02:29 +0800

    提交

--- src/main/java/org/leetcode/shunxu/leet2000/ch1600/Ch1592RearrangeSpacesBetweenWords.java
+++ src/main/java/org/leetcode/shunxu/leet2000/ch1600/Ch1592RearrangeSpacesBetweenWords.java
@@ -1,9 +1,61 @@
 package org.leetcode.shunxu.leet2000.ch1600;
 
 /**
- * @author: wangrui
- * @date: 2021/4/22
+ * <p>1592. 重新排列单词间的空格
+ *
+ * <p>给你一个字符串 text ，该字符串由若干被空格包围的单词组成。每个单词由一个或者多个小写英文字母组成，并且两个单词之间至少存在一个空格。题目测试用例保证 text 至少包含一个单词 。
+ *
+ * <p>请你重新排列空格，使每对相邻单词之间的空格数目都 相等 ，并尽可能 最大化 该数目。如果不能重新平均分配所有空格，请 将多余的空格放置在字符串末尾 ，这也意味着返回的字符串应当与原 text 字符串的长度相等。
+ *
+ * <p>返回 重新排列空格后的字符串 。
+ *
+ *
+ *
+ * <p>示例 1：
+ *
+ * <p>输入：text = "  this   is  a sentence "
+ * <p>输出："this   is   a   sentence"
+ * <p>解释：总共有 9 个空格和 4 个单词。可以将 9 个空格平均分配到相邻单词之间，相邻单词间空格数为：9 / (4-1) = 3 个。
+ *
+ * <p>示例 2：
+ *
+ * <p>输入：text = " practice   makes   perfect"
+ * <p>输出："practice   makes   perfect "
+ * <p>解释：总共有 7 个空格和 3 个单词。7 / (3-1) = 3 个空格加上 1 个多余的空格。多余的空格需要放在字符串的末尾。
+ *
+ * <p>示例 3：
+ *
+ * <p>输入：text = "hello   world"
+ * <p>输出："hello   world"
+ *
+ * <p>示例 4：
+ *
+ * <p>输入：text = "  walks  udp package   into  bar a"
+ * <p>输出："walks  udp  package  into  bar  a "
+ *
+ * <p>示例 5：
+ *
+ * <p>输入：text = "a"
+ * <p>输出："a"
+ *
+ *
+ *
+ * <p>提示：
+ *
+ * <p>    1 <= text.length <= 100
+ * <p>    text 由小写英文字母和 ' ' 组成
+ * <p>    text 中至少包含一个单词
+ *
+ * <p>通过次数6,450
+ * <p>提交次数14,461
+ * <p>请问您在哪类招聘中遇到此题？
+ * <p>https://leetcode-cn.com/problems/rearrange-spaces-between-words/
+ * <p>@author: wangrui
+ * <p>@date: 2021/4/22
  */
 public class Ch1592RearrangeSpacesBetweenWords {
 
+  public String reorderSpaces(String text) {
+    return "";
+  }
 }
--- src/main/java/org/leetcode/shunxu/leet2000/ch1650/Ch1603DesignParkingSystem.java
+++ src/main/java/org/leetcode/shunxu/leet2000/ch1650/Ch1603DesignParkingSystem.java
@@ -1,9 +1,60 @@
 package org.leetcode.shunxu.leet2000.ch1650;
 
 /**
- * @author: wangrui
- * @date: 2021/4/22
+ * <p>1603. 设计停车系统
+ *
+ * <p>请你给一个停车场设计一个停车系统。停车场总共有三种不同大小的车位：大，中和小，每种尺寸分别有固定数目的车位。
+ *
+ * <p>请你实现 ParkingSystem 类：
+ *
+ * <p>    ParkingSystem(int big, int medium, int small) 初始化 ParkingSystem 类，三个参数分别对应每种停车位的数目。
+ * <p>    bool addCar(int carType) 检查是否有 carType 对应的停车位。 carType 有三种类型：大，中，小，分别用数字 1， 2 和 3 表示。一辆车只能停在  carType
+ * 对应尺寸的停车位中。如果没有空车位，请返回 false ，否则将该车停入车位并返回 true 。
+ *
+ *
+ *
+ * <p>示例 1：
+ *
+ * <p>输入：
+ * <p>["ParkingSystem", "addCar", "addCar", "addCar", "addCar"]
+ * <p>[[1, 1, 0], [1], [2], [3], [1]]
+ * <p>输出：
+ * <p>[null, true, true, false, false]
+ *
+ * <p>解释：
+ * <p>ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+ * <p>parkingSystem.addCar(1); // 返回 true ，因为有 1 个空的大车位
+ * <p>parkingSystem.addCar(2); // 返回 true ，因为有 1 个空的中车位
+ * <p>parkingSystem.addCar(3); // 返回 false ，因为没有空的小车位
+ * <p>parkingSystem.addCar(1); // 返回 false ，因为没有空的大车位，唯一一个大车位已经被占据了
+ *
+ *
+ *
+ * <p>提示：
+ *
+ * <p>    0 <= big, medium, small <= 1000
+ * <p>    carType 取值为 1， 2 或 3
+ * <p>    最多会调用 addCar 函数 1000 次
+ *
+ * <p>通过次数51,060
+ * <p>提交次数60,196
+ * <p>请问您在哪类招聘中遇到此题？
+ * <p>https://leetcode-cn.com/problems/design-parking-system/
+ * <p>@author: wangrui
+ * <p>@date: 2021/4/22
  */
 public class Ch1603DesignParkingSystem {
 
+  public Ch1603DesignParkingSystem(int big, int medium, int small) {
+
+  }
+
+  public boolean addCar(int carType) {
+    return true;
+  }
 }
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such: ParkingSystem obj = new ParkingSystem(big, medium,
+ * small); boolean param_1 = obj.addCar(carType);
+ */
