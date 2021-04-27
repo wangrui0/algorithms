abff176c9706a2b2cba73fa170db2a35dd45952b
wangrui <wangrui@limikeji.com>
2021-04-27 13:53:30 +0800

    提交

---
+++ src/main/java/org/leetcode/leet2000/ch1700/Ch1668MaximumRepeatingSubstring.java
@@ -0,0 +1,70 @@
+package org.leetcode.leet2000.ch1700;
+
+/**
+ * <p>1668. 最大重复子字符串
+ * <p>给你一个字符串 sequence ，如果字符串 word 连续重复 k 次形成的字符串是 sequence 的一个子字符串，那么单词 word 的 重复值为 k 。单词 word 的 最大重复值 是单词 word 在
+ * sequence 中最大的重复值。如果 word 不是 sequence 的子串，那么重复值 k 为 0 。
+ *
+ * <p>给你一个字符串 sequence 和 word ，请你返回 最大重复值 k 。
+ *
+ *
+ *
+ * <p>示例 1：
+ *
+ * <p>输入：sequence = "ababc", word = "ab"
+ * <p>输出：2
+ * <p>解释："abab" 是 "ababc" 的子字符串。
+ * <p>示例 2：
+ *
+ * <p>输入：sequence = "ababc", word = "ba"
+ * <p>输出：1
+ * <p>解释："ba" 是 "ababc" 的子字符串，但 "baba" 不是 "ababc" 的子字符串。
+ * <p>示例 3：
+ *
+ * <p>输入：sequence = "ababc", word = "ac"
+ * <p>输出：0
+ * <p>解释："ac" 不是 "ababc" 的子字符串。
+ *
+ *
+ * <p>提示：
+ *
+ * <p>1 <= sequence.length <= 100
+ * <p>1 <= word.length <= 100
+ * <p>sequence 和 word 都只包含小写英文字母。
+ * <p>通过次数4,823提交次数11,163
+ * <p>https://leetcode-cn.com/problems/maximum-repeating-substring/
+ * <p>@Author: shenpei
+ * <p>@Date: 2021/4/25 7:31 下午
+ */
+public class Ch1668MaximumRepeatingSubstring {
+
+  public int maxRepeating(String sequence, String word) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(word);
+    int count = 0;
+    while (sequence.contains(sb)) {
+      count++;
+      sb.append(word);
+    }
+    return count;
+
+  }
+
+  public int maxRepeating_2(String sequence, String word) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(word);
+    int count = 0;
+    int start = 0;
+    for (int i = start; i < sequence.length() - sb.length(); i++) {
+      if (sequence.charAt(i) == sb.charAt(i)) {
+        i++;
+      }
+    }
+    while (sequence.contains(sb)) {
+      count++;
+      sb.append(word);
+    }
+    return count;
+
+  }
+}
