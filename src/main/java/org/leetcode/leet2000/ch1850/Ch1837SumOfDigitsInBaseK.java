a4c10de3c3842b9db0517f1f4c63595d08642655
wangrui <wangrui@limikeji.com>
2021-04-29 11:48:35 +0800

    提交

--- src/main/java/org/leetcode/leet2000/ch1850/Ch1837SumOfDigitsInBaseK.java
+++ src/main/java/org/leetcode/leet2000/ch1850/Ch1837SumOfDigitsInBaseK.java
@@ -35,9 +35,37 @@ package org.leetcode.leet2000.ch1850;
  * <p>@Author: shenpei
  * <p>@Date: 2021/4/25 10:21 下午
  */
-public class NCh1837SumOfDigitsInBaseK {
+public class Ch1837SumOfDigitsInBaseK {
 
+  /**
+   * <p>复杂度分析
+   *
+   * <p>    时间复杂度：O(logn)，循环的次数与 n 在 k 进制下的位数相同。
+   *
+   * <p>    空间复杂度：O(1)。
+   *
+   * @param n
+   * @param k
+   * @return
+   */
   public int sumBase(int n, int k) {
-    return -1;
+    int ans = 0;
+    while (n > 0) {
+      ans += n % k;
+      n /= k;
+    }
+    return ans;
   }
+
+//  class Solution {
+//    public:
+//    int sumBase(int n, int k) {
+//      int res = 0;
+//      while (n){
+//        res += n % k;
+//        n /= k;
+//      }
+//      return res;
+//    }
+//  };
 }
